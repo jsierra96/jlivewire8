851d8e8168c4d22ae7854c5e00c30b1b5f81fd31
vagrant <vagrant@homestead>
2022-10-07 22:51:05 +0000

    Configuracion de factories y seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+use App\Models\Course;
+use App\Models\Post;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +18,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::factory(5)->create();
+        Category::factory(3)->create();
+        Course::factory(10)->create();
+        Post::factory(60)->create();
     }
 }
