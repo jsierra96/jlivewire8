91c57788175830c2c2a64c1d081fd3b25be6630f
Joanan Sierra S <joanan96@gmail.com>
2022-10-25 22:38:57 -0500

    Index y Layout: definiendo estructura HTM

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.web')
+
+@section('content')
+<div class="text-center">
+    <h1 class="text-3xl text-gray-700 mb-2 uppercase">Ultimos cursos</h1>
+    <h2 class="text-xl text-gray-600">Formate online como profesional de la tecnología</h2>
+    <h3 class="text-lg text-gray-600">70% de los egresados duplica sus ingresos</h3>
+</div>
+@endsection
